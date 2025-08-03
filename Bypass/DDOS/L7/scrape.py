610b5a93f9cb82964aba98246dcd1296b9af4e65
illusionsec <illusionsec01@proton.me>
2025-08-03 12:23:34 +0300

    Bypass

---
+++ Bypass/DDOS/L7/scrape.py
@@ -0,0 +1,143 @@
+import requests
+import os
+import random
+import re
+import threading
+import urllib.request
+import argparse
+import sys
+from colorama import Fore, init
+from concurrent.futures import ThreadPoolExecutor
+from time import time
+
+init(autoreset=True)
+
+output_file = 'proxy.txt'
+os.system('cls' if os.name == 'nt' else 'clear')
+
+if os.path.isfile(output_file):
+    os.remove(output_file)
+
+proxy_urls = [
+'https://api.openproxylist.xyz/http.txt',
+'https://api.proxyscrape.com/v2/?request=displayproxies',
+'https://raw.githubusercontent.com/officialputuid/KangProxy/KangProxy/http/http.txt',
+'https://raw.githubusercontent.com/roosterkid/openproxylist/main/HTTPS_RAW.txt',
+'https://raw.githubusercontent.com/yuceltoluyag/GoodProxy/main/raw.txt',
+'https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/http.txt',
+'https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/https.txt',
+'https://raw.githubusercontent.com/mmpx12/proxy-list/master/https.txt',
+'https://proxyspace.pro/http.txt',
+'https://api.proxyscrape.com/?request=displayproxies&proxytype=http',
+'https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt',
+'http://worm.rip/http.txt',
+'http://alexa.lr2b.com/proxylist.txt',
+'https://api.openproxylist.xyz/http.txt',
+'http://rootjazz.com/proxies/proxies.txt',
+'https://multiproxy.org/txt_all/proxy.txt',
+'https://proxy-spider.com/api/proxies.example.txt',
+'https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt',
+'https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies.txt',
+'https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt',
+'https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt',
+'https://raw.githubusercontent.com/sunny9577/proxy-scraper/master/proxies.txt',
+'https://raw.githubusercontent.com/opsxcq/proxy-list/master/list.txt',
+'https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all',
+'https://www.proxydocker.com/en/proxylist/download?email=noshare&country=all&city=all&port=all&type=all&anonymity=all&state=all&need=all',
+'https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=anonymous',
+'https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all',
+]
+
+
+class Proxy:
+    def __init__(self, method, proxy):
+        if method.lower() not in ["http", "https"]:
+            raise NotImplementedError("Only HTTP and HTTPS are supported")
+        self.method = method.lower()
+        self.proxy = proxy
+
+    def is_valid(self):
+        return re.match(r"\d{1,3}(?:\.\d{1,3}){3}(?::\d{1,5})?$", self.proxy)
+
+    def check(self, site, timeout, user_agent):
+        url = self.method + "://" + self.proxy
+        proxy_support = urllib.request.ProxyHandler({self.method: url})
+        opener = urllib.request.build_opener(proxy_support)
+        urllib.request.install_opener(opener)
+        req = urllib.request.Request(self.method + "://" + site)
+        req.add_header("User-Agent", user_agent)
+        try:
+            start_time = time()
+            urllib.request.urlopen(req, timeout=timeout)
+            end_time = time()
+            time_taken = end_time - start_time
+            return True, time_taken, None
+        except Exception as e:
+            return False, 0, e
+
+    def __str__(self):
+        return self.proxy
+    
+def download_and_save_proxies(url, output_file):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            with open(output_file, 'a') as file:
+                file.write(response.text)
+                print(f"\x1b[38;5;160mSuccessfully Collecting Proxy (\x1b[38;5;255mD4XG API\x1b[38;5;160m)")
+        else:
+            print(f"{Fore.RED}Gagal {url}{Fore.RESET}")
+    except Exception as e:
+        print(f"{Fore.RED}Gagal {url}{Fore.RESET}")
+
+def check_proxy(proxy, user_agent, site, timeout, verbose):
+    new_user_agent = user_agent
+    if args.random_agent:
+        new_user_agent = random.choice(user_agents)
+    valid, time_taken, error = proxy.check(site, timeout, new_user_agent)
+    message = {
+        True: f"{proxy} is valid, took {time_taken} seconds",
+        False: f"{proxy} is invalid: {repr(error)}",
+    }[valid]
+    verbose_print(verbose, message)
+    return [proxy] if valid else []
+
+def check(file, timeout, method, site, verbose, random_user_agent):
+    proxies = []
+    with open(file, "r") as f:
+        for line in f:
+            proxies.append(Proxy(method, line.replace("\n", "")))
+
+    print(f"{Fore.GREEN}Checking {Fore.YELLOW}{len(proxies)} {Fore.GREEN}Proxy")
+    proxies = filter(lambda x: x.is_valid(), proxies)
+
+    valid_proxies = []
+    user_agent = random.choice(user_agents)
+
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        results = executor.map(
+            lambda proxy: check_proxy(proxy, user_agent, site, timeout, verbose),
+            proxies
+        )
+
+        for result_list in results:
+            valid_proxies.extend(result_list)
+
+    with open(file, "w") as f:
+        for proxy in valid_proxies:
+            f.write(str(proxy) + "\n")
+
+    print(f"{Fore.GREEN}Found {Fore.YELLOW}{len(valid_proxies)} {Fore.GREEN}valid proxies")
+
+
+def verbose_print(verbose, message):
+    if verbose:
+        print(message)
+
+for url in proxy_urls:
+    download_and_save_proxies(url, output_file)
+    
+with open('proxy.txt', 'r') as ceki:
+    jumlh = sum(1 for line in ceki)
+    
+print(f"✦ ━━━━━ ★ ━━━━━ ✦\n\x1b[38;5;160mCongralutions You Got ( \x1b[38;5;255m{jumlh} \x1b[38;5;160m) Proxies")
